8702e7dae364f2656ff9a64a0a23894ff95f2ab0
captnseagraves <k.s.seagraves@gmail.com>
2024-07-19 17:02:32 -0400

    resolving dependency issues. eigens deps aren't functioning now

--- contracts/script/HoleskyDeployer.s.sol
+++ contracts/script/HoleskyDeployer.s.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+
 import "@eigenlayer/contracts/permissions/PauserRegistry.sol";
 import {IDelegationManager} from "@eigenlayer/contracts/interfaces/IDelegationManager.sol";
 import {IAVSDirectory} from "@eigenlayer/contracts/interfaces/IAVSDirectory.sol";
